cb05a3f69972f072d99551a0d80fb9bf7fda37bd
MalinJ123 <malin--jonsson@hotmail.com>
2023-10-18 23:19:23 +0200

    now you can se how many language that exist in the json file

--- src/data/getConfig.js
+++ src/data/getConfig.js
@@ -1,25 +1,53 @@
+
+
 import data from "./movieFlex.json";
 
 console.log("getting all movies", data);
 
-export function getConfig() {
 
-  const specialsCount = data.Specials.length;
-  const featureCount = data.Feature.length;
-  const dokumentariesCount = data.Dokumentaries.length;
+//EN FUNKTION FÖR ATT HÄMTA ALLA SPRÅKEN I : Specials", "Feature", "Dokumentaries 
+
+export function getConfig() {
+  const languageCounts = {
+    English: 0,
+    Spanish: 0,
+    Polish: 0,
+    Tamil: 0
+    // Add more languages as needed
+  };
+
+  // Count the languages for each category
+  data.Specials.forEach(movie => {
+    languageCounts[movie.Language]++;
+  });
+
+  data.Feature.forEach(movie => {
+    languageCounts[movie.Language]++;
+  });
+
+  data.Dokumentaries.forEach(movie => {
+    languageCounts[movie.Language]++;
+  });
+
+  const labels = Object.keys(languageCounts);
+  const counts = Object.values(languageCounts);
 
   return {
-    labels: ["Specials", "Feature", "Dokumentaries"],
+    labels,
     datasets: [
       {
-        label: "All Movies Counts",
-        data: [specialsCount, featureCount, dokumentariesCount],
+        label: "Language Counts",
+        data: counts,
         backgroundColor: [
           "rgba(255, 99, 132, 0.8)",
           "rgba(54, 162, 235, 0.8)",
           "rgba(255, 206, 86, 0.8)",
+          "rgba(75, 192, 192, 0.8)"
+          // Add more colors as needed
         ],
       },
     ],
   };
 }
+
+
